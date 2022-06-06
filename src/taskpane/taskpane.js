e4183596309d099b0af689dd78f05d2767fa0c28
Frank-masciopinto <francesco.masciopinto@outlook.com>
2022-06-06 17:04:36 +0700

    Changed Npm version to 16, fixed localhost error

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -5,6 +5,11 @@
 
 /* global document, Office */
 
+Office.initialize = () => {
+  console.log("Initialized")
+
+};
+
 Office.onReady((info) => {
   if (info.host === Office.HostType.Outlook) {
     document.getElementById("sideload-msg").style.display = "none";
@@ -14,7 +19,9 @@ Office.onReady((info) => {
 });
 
 export async function run() {
-  /**
-   * Insert your Outlook code here
-   */
+  // Get a reference to the current message
+var item = Office.context.mailbox.item;
+
+// Write message property value to the task pane
+document.getElementById("item-subject").innerHTML = "<b>Subject:</b> <br/>" + item.subject;
 }
